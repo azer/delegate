706c25a08284b7b88841541f709af91e8d6b0e40
Finn <derfinn@gmail.com>
2015-05-03 17:04:46 +0200

    call event for parent elements

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var test = require('prova');
 var on = require("./");
 
-document.body.innerHTML = '<ul><li><a>Foo</a></li><li>Bar</li><li>Qux</li></ul>';
+document.body.innerHTML = '<ul><li><a><img/>Foo</a></li><li>Bar</li><li>Qux</li></ul>';
 
 test('adds event to specified selector', function (t) {
   t.plan(1);
@@ -13,4 +13,17 @@ test('adds event to specified selector', function (t) {
   });
 
   document.querySelector('ul li a').click();
+});
+
+test('calls event of parent element', function (t) {
+  t.plan(1);
+
+  var fn = on(document.body, 'ul li a', 'click', function (e) {
+    on.off(document.body, 'click', fn);
+    document.querySelector('ul li a img').click();
+    document.querySelector('ul li a').click();
+    t.ok(true);
+  });
+
+  document.querySelector('ul li a img').click();
 });
